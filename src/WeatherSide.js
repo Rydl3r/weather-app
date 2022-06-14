eb6964083134450f5d94223e6bf231625ca57880
Ivan Mukoed <1rydler@gmail.com>
2022-06-14 17:32:42 +0300

    syntax fixes

--- src/WeatherSide.js
+++ src/WeatherSide.js
@@ -1,34 +1,36 @@
-import React from 'react'
+import React from "react";
 
-const WeatherSide = ({city, weather}) => {
+const WeatherSide = ({ city, weather }) => {
+  function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
 
-    function capitalizeFirstLetter(string) {
-        return string.charAt(0).toUpperCase() + string.slice(1);
-    }
+  let today = new Date().toISOString().slice(0, 10);
+  let dateObj = new Date();
+  let weekday = capitalizeFirstLetter(
+    dateObj.toLocaleString("default", { weekday: "long" })
+  );
+  let imgLink = `http://openweathermap.org/img/w/${weather.weather[0].icon}.png`;
+  const imgStyle = { width: "100px" };
 
-    let today = new Date().toISOString().slice(0, 10)
-    let dateObj = new Date()
-    let weekday = capitalizeFirstLetter(dateObj.toLocaleString("default", { weekday: "long" }))
-    let imgLink = `http://openweathermap.org/img/w/${weather.weather[0].icon}.png`
-    const imgStyle = {  width: "100px" }
-       
-    return (
-        <div className="weather-side">
-        <div className="weather-gradient"></div>
-        <div className="date-container">
-          <h2 className="date-dayname">{weekday}</h2> <span className="date-day">{today}</span><i className="location-icon" data-feather="map-pin"></i><span className="location">{city}</span> </div>
-        <div className="weather-container"> <i className="weather-icon" data-feather="sun"></i>
-          <img style={imgStyle} src={imgLink}/>
-          <h1 className="weather-temp">{weather.main.temp}</h1>
-          <h3 className="weather-desc">{weather.weather[0].description}</h3> </div>
+  return (
+    <div className="weather-side">
+      <div className="weather-gradient"></div>
+      <div className="date-container">
+        <h2 className="date-dayname">{weekday}</h2>{" "}
+        <span className="date-day">{today}</span>
+        <i className="location-icon" data-feather="map-pin"></i>
+        <span className="location">{city}</span>{" "}
       </div>
+      <div className="weather-container">
+        {" "}
+        <i className="weather-icon" data-feather="sun"></i>
+        <img style={imgStyle} src={imgLink} />
+        <h1 className="weather-temp">{weather.main.temp}</h1>
+        <h3 className="weather-desc">{weather.weather[0].description}</h3>{" "}
+      </div>
+    </div>
+  );
+};
 
-
-
-
-
-
-    )
-}
-
-export default WeatherSide
+export default WeatherSide;
